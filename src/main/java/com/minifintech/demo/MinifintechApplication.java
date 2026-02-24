6d277c10636aca53d5857a5cabaf51b451ae96be
qsngoseDev <qsngoaiwork@gmail.com>
2026-02-24 22:17:36 +0700

    connected to database

---
+++ src/main/java/com/minifintech/demo/MinifintechApplication.java
@@ -0,0 +1,13 @@
+package com.minifintech.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinifintechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinifintechApplication.class, args);
+	}
+
+}
